ebed8907c714e53fb509011bb83bbc51ed9d86d9
kelvinfloresta <kelvinf.contato@gmail.com>
2022-07-07 02:13:55 -0300

    feat: add format module

---
+++ src/module/format.ts
@@ -0,0 +1,9 @@
+const currency = Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+});
+
+export function formatCurrency(value: number) {
+  return currency.format(value);
+}
