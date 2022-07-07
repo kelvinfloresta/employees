65139b6e6d73cf6e50f9e0c1c536262f71259406
kelvinfloresta <kelvinf.contato@gmail.com>
2022-07-07 01:24:06 -0300

    feat: add useListEmployee hook

---
+++ src/hook/useListEmployee.ts
@@ -0,0 +1,34 @@
+import { useCallback, useState } from "react";
+import { employeeService } from "../service/employee";
+import { ListEmployeeOutput } from "../service/employee.interface";
+
+export function useListEmployee() {
+  const [employees, setEmployees] = useState<ListEmployeeOutput[]>([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const listEmployee = useCallback(() => {
+    const sub = employeeService.list();
+
+    setLoading(true);
+    setError(null);
+
+    sub.subscribe({
+      next(employees) {
+        setLoading(false);
+        setEmployees(employees.data.data);
+      },
+      error(error) {
+        setLoading(false);
+        setError(error);
+      },
+    });
+  }, []);
+
+  return {
+    listEmployee,
+    employees,
+    error,
+    loading,
+  };
+}
