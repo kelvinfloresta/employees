bdead38dec10d91e0ea86739617b42d91743bce2
kelvinfloresta <kelvinf.contato@gmail.com>
2022-07-07 02:15:05 -0300

    feat: add List component

---
+++ src/component/List.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+
+export const List = styled.div`
+    margin: 1rem 0;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 2rem;
+`
