d9203c0353412f97ef4247f4781bb4b67db98c6a
kelvinfloresta <kelvinf.contato@gmail.com>
2022-07-07 01:25:13 -0300

    feat(App): add employee list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Employee } from './component/Employee';
+import { useListEmployee } from './hook/useListEmployee';
 
 function App() {
+  const {employees, listEmployee, error, loading} = useListEmployee()
+
+  useEffect(listEmployee, [listEmployee])
+
+  if (loading) {
+    return <div>Loading employees...</div>
+  }
+
+  if (error) {
+    return <div>
+      <div>An error has occurred!</div>
+      <p>Click <button onClick={listEmployee}>here</button> and try again</p>
+    </div>
+  }
+
   return (
-    <h1>App</h1>
+    <>
+    {employees.map(({
+      id,
+      employee_age,
+      employee_name,
+      employee_salary,
+      profile_image,
+    }) => {
+      return <Employee
+        key={id}
+        id={id}
+        employee_age={employee_age}
+        employee_name={employee_name}
+        employee_salary={employee_salary}
+        profile_image={profile_image}
+      />
+    })}
+    </>
   );
 }
 
